eaddae175262fa376c8fd9573f6d61198d3fafb2
FullerJam <2fullj37@solent.ac.uk>
2020-09-13 12:43:41 +0100

    user data now passing to react-modal, adding styling currently, will need to change structure before filter logic possibly

--- src/app/components/Users/Users.jsx
+++ src/app/components/Users/Users.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
+
 import styled from "styled-components";
 import circleArrowIcon from "../../../../public/icons/circle-arrow.png";
+import avatarPlaceholder from "../../../../public/icons/avatar_placeholder.png";
 // import theme from "../../config/theme";
 
 const StyledUsersWrapper = styled.div`
@@ -15,15 +17,15 @@ const StyledWrapper = styled.div``;
 
 const StyledUserTile = styled.div`
   display: flex;
-  padding:0 50px 0 50px;
+  padding: 0 50px 0 50px;
   justify-content: space-between;
-  align-items:center;
+  align-items: center;
   min-width: 800px;
   border: 1px grey solid;
-  border-radius: 5px; 
+  border-radius: 5px;
   margin: 10px;
   padding: 10px;
-  background-color:#f4f4f4;
+  background-color: #f4f4f4;
   p {
     margin-left: 8px;
     font-family: helvetica;
@@ -42,41 +44,84 @@ const Button = styled.button`
   }
 `;
 
-const StyledAvatar = styled.div`
-  padding: 4px;
-  background-color: white;
-  border:.5px solid grey;
+const StyledAvatar = styled.img`
   border-radius: 50%;
-  min-width:73px;
-  img{
-    border-radius: 50%;
-    position: relative;
-    bottom:-2px;
-  }
+  position: relative;
+  border: 5px solid white;
+`;
+
+const StyledAvatar2 = styled.img`
+  border-radius: 50%;
+  position: relative;
+  border: 5px solid white;
+`;
+
+const StyledError = styled.p`
+  color: red;
+  font-weight: bold;
+  font-size: 25px;
 `;
 
 const UserInfoWrapper = styled.div`
   display: flex;
-  align-items:center;
+  align-items: center;
+`;
+
+const StyledList = styled.li`
+  text-decoration:none;
 `;
 
 function Users(props) {
+  const [selectedUser, setSelectedUser] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const { users, errors } = props;
+
+  const openModal = (user) => {
+    setModalIsOpen(!modalIsOpen);
+    setSelectedUser(user);
+    console.log("selected User " + selectedUser);
+  };
 
   return (
     <React.Fragment>
+      {errors && <StyledError>{errors}</StyledError>}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          content: {
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "center",
+          },
+          overlay: {
+            backgroundColor: "#71090D",
+          },
+        }}
+      >
+        <StyledAvatar2
+          src={modalIsOpen && selectedUser.picture.large}
+          alt="userAvatar"
+        />
+        <p>
+          {modalIsOpen &&
+            selectedUser.name.first + " " + selectedUser.name.last}
+        </p>
+      </Modal>
       <StyledUsersWrapper>
         <StyledWrapper>
           {users.map((user) => (
             <StyledUserTile>
               <UserInfoWrapper>
-                <StyledAvatar>
-                  <img src={user.picture.medium} alt="userAvatar"/>
-                </StyledAvatar>
-          <p>{user.name.first}</p>
+                <StyledAvatar
+                  src={user.picture.medium || avatarPlaceholder}
+                  alt="userAvatar"
+                />
+                <p>{user.name.first}</p>
                 <p>{user.name.last}</p>
               </UserInfoWrapper>
-              <Button id={user.login.uuid}/>
+              <Button id={user.login.uuid} onClick={() => openModal(user)} />
             </StyledUserTile>
           ))}
         </StyledWrapper>
